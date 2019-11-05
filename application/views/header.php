1c79cde93740362d794fb968575ef5f8e101e24d
Aramis-Delg <aramis_de.mo@hotmail.com>
2019-11-05 14:13:58 -0600

    Notificacion de cerrar sesión

--- application/views/header.php
+++ application/views/header.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="<?php confirm("Press a button!"); echo base_url()?>index.php/Administrador/cerrar_sesion">Cerrar Sesión</a></li>
+    <li class="breadcrumb-item"><a id='enlace' onclick="myFunction();" href="#">Cerrar Sesión</a></li>
     <li class="breadcrumb-item active" aria-current="page"><?php echo $this->session->userdata('s_nombre');?></li>
   </ol>
 </nav>
@@ -33,7 +33,7 @@ function myFunction() {
 
   var r = confirm("¿Estás seguro de que quieres salir de la prueba?, tu avance se perderá. ");
   if (r == true) {
-    
+    document.getElementById('enlace').setAttribute('href',"<?php echo base_url()?>index.php/Administrador/cerrar_sesion/");
 
   } else {
     
